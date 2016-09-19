ef7847b628bdb4dda2190322ee22d0d4d650e738
Ichiba Tamotsu <ichibat@gmail.com>
2016-09-19 20:37:25 +0900

    8回目の課題

---
+++ 8/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>POSTデータ登録</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <style>div{padding: 10px;font-size:16px;}</style>
+</head>
+<body>
+
+<!-- Head[Start] -->
+<header>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+    <div class="navbar-header"><a class="navbar-brand" href="select.php">データ一覧</a></div>
+  </nav>
+</header>
+<!-- Head[End] -->
+
+<!-- Main[Start] -->
+<form method="post" action="insert.php">
+  <div class="jumbotron">
+   <fieldset>
+    <legend>ブックマーク</legend>
+     <label>本の名前：<input type="text" name="book_name"></label><br>
+     <label>本のurl：<input type="text" name="book_url"></label><br>
+     <label>本のコメント：<textArea name="book_cmt" rows="4" cols="40"></textArea></label><br>
+     <input type="submit" value="送信">
+    </fieldset>
+  </div>
+</form>
+<!-- Main[End] -->
+
+
+</body>
+</html>
